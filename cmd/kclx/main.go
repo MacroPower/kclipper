00aee4448f6f6b66ebd42b7ea12214ab413fcebe
Jacob Colvin <jacobcolvin1@gmail.com>
2024-12-19 22:58:43 -0500

    Add plugin support to CLI

--- cmd/kclx/main.go
+++ cmd/kclx/main.go
@@ -10,7 +10,8 @@ import (
 	"strings"
 
 	"github.com/spf13/cobra"
-	cmd "kcl-lang.io/cli/cmd/kcl/commands"
+	kclcmd "kcl-lang.io/cli/cmd/kcl/commands"
+	"kcl-lang.io/cli/pkg/plugin"
 
 	_ "kcl-lang.io/kcl-plugin/http"
 
@@ -30,7 +31,7 @@ scenarios. The KCL website: https://kcl-lang.io
 )
 
 func main() {
-	cmdx := &cobra.Command{
+	cmd := &cobra.Command{
 		Use:           cmdName,
 		Short:         shortDesc,
 		Long:          longDesc,
@@ -38,21 +39,90 @@ func main() {
 		SilenceErrors: true,
 		Version:       GetVersionString(),
 	}
-	cmdx.AddCommand(cmd.NewRunCmd())
-	cmdx.AddCommand(cmd.NewLintCmd())
-	cmdx.AddCommand(cmd.NewDocCmd())
-	cmdx.AddCommand(cmd.NewFmtCmd())
-	cmdx.AddCommand(cmd.NewTestCmd())
-	cmdx.AddCommand(cmd.NewVetCmd())
-	cmdx.AddCommand(cmd.NewCleanCmd())
-	cmdx.AddCommand(cmd.NewImportCmd())
-	cmdx.AddCommand(cmd.NewModCmd())
-	cmdx.AddCommand(cmd.NewRegistryCmd())
-	cmdx.AddCommand(cmd.NewServerCmd())
-	cmdx.AddCommand(NewVersionCmd())
+	cmd.AddCommand(kclcmd.NewRunCmd())
+	cmd.AddCommand(kclcmd.NewLintCmd())
+	cmd.AddCommand(kclcmd.NewDocCmd())
+	cmd.AddCommand(kclcmd.NewFmtCmd())
+	cmd.AddCommand(kclcmd.NewTestCmd())
+	cmd.AddCommand(kclcmd.NewVetCmd())
+	cmd.AddCommand(kclcmd.NewCleanCmd())
+	cmd.AddCommand(kclcmd.NewImportCmd())
+	cmd.AddCommand(kclcmd.NewModCmd())
+	cmd.AddCommand(kclcmd.NewRegistryCmd())
+	cmd.AddCommand(kclcmd.NewServerCmd())
+	cmd.AddCommand(NewVersionCmd())
+	bootstrapCmdPlugin(cmd, plugin.NewDefaultPluginHandler([]string{cmdName}))
 
-	if err := cmdx.Execute(); err != nil {
+	if err := cmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, strings.TrimLeft(err.Error(), "\n"))
 		os.Exit(1)
+	}
+}
+
+// executeRunCmd the run command for the root command.
+func executeRunCmd(args []string) {
+	cmd := kclcmd.NewRunCmd()
+	cmd.SetArgs(args)
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func isHelpOrVersionFlag(flag string) bool {
+	return flag == "-h" || flag == "--help" || flag == "-v" || flag == "--version"
+}
+
+func bootstrapCmdPlugin(cmd *cobra.Command, pluginHandler plugin.PluginHandler) {
+	if pluginHandler == nil {
+		return
+	}
+	if len(os.Args) <= 1 {
+		return
+	}
+	cmdPathPieces := os.Args[1:]
+
+	// only look for suitable extension executables if
+	// the specified command does not already exist
+	// flags cannot be placed before plugin name
+	if strings.HasPrefix(cmdPathPieces[0], "-") && !isHelpOrVersionFlag(cmdPathPieces[0]) {
+		executeRunCmd(cmdPathPieces)
+		return
+	}
+
+	foundCmd, _, err := cmd.Find(cmdPathPieces)
+	if err == nil {
+		return
+	}
+
+	// Also check the commands that will be added by Cobra.
+	// These commands are only added once rootCmd.Execute() is called, so we
+	// need to check them explicitly here.
+	var cmdName string // first "non-flag" arguments
+	for _, arg := range cmdPathPieces {
+		if !strings.HasPrefix(arg, "-") {
+			cmdName = arg
+			break
+		}
+	}
+
+	builtinSubCmdExist := false
+	for _, cmd := range foundCmd.Commands() {
+		if cmd.Name() == cmdName {
+			builtinSubCmdExist = true
+			break
+		}
+	}
+	switch cmdName {
+	// Don't search for a plugin
+	case "help", "completion", cobra.ShellCompRequestCmd, cobra.ShellCompNoDescRequestCmd:
+	default:
+		if !builtinSubCmdExist {
+			if err := plugin.HandlePluginCommand(pluginHandler, cmdPathPieces, false); err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+			executeRunCmd(cmdPathPieces)
+		}
 	}
 }
