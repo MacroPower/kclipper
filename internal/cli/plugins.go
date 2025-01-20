bb9ef66837500cc05d038f7f0335be6df400bfdd
Jacob Colvin <jacobcolvin1@gmail.com>
2025-01-20 00:00:09 -0500

    Remove `http` and `os` plugins

--- internal/cli/plugins.go
+++ internal/cli/plugins.go
@@ -5,19 +5,11 @@ import (
 	"strings"
 
 	helmplugin "github.com/MacroPower/kclipper/pkg/plugin/helm"
-	httpplugin "github.com/MacroPower/kclipper/pkg/plugin/http"
-	osplugin "github.com/MacroPower/kclipper/pkg/plugin/os"
 )
 
 func RegisterEnabledPlugins() {
 	if !envTrue("KCLX_HELM_PLUGIN_DISABLED") {
 		helmplugin.Register()
-	}
-	if !envTrue("KCLX_HTTP_PLUGIN_DISABLED") {
-		httpplugin.Register()
-	}
-	if !envTrue("KCLX_OS_PLUGIN_DISABLED") {
-		osplugin.Register()
 	}
 }
 
