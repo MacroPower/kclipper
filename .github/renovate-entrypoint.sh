7bdd2eb3950028d82eceaa173ad4c4809deabf16
Jacob Colvin <jacobcolvin1@gmail.com>
2025-01-24 23:33:00 -0500

    Add kclipper CLI to renovate

---
+++ .github/renovate-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+KCLIPPER_URL=$(curl -s "https://api.github.com/repos/MacroPower/kclipper/releases/latest" | \
+  jq -r ".assets[] | select(.name | test(\"kclipper_$(uname)_$(arch).tar.gz\")) | .browser_download_url")
+
+echo "Downloading kclipper from $KCLIPPER_URL"
+curl -L $KCLIPPER_URL | tar -zx
+
+chmod +x kcl
+mv kcl /usr/local/bin
+
+runuser -u ubuntu renovate
