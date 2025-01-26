7d3ce40566c25986aef34bc61c08288d45ad0017
Jacob Colvin <jacobcolvin1@gmail.com>
2025-01-26 15:44:26 -0500

    Improve package root detection

--- pkg/kclutil/repo_root.go
+++ pkg/kclutil/repo_root.go
@@ -14,19 +14,65 @@ var (
 )
 
 // FindTopPkgRoot finds the topmost `kcl.mod` file for the provided path. It is
-// similar to [kcl-lang.io/kcl-go/pkg/utils.FindPkgRoot], but climbs as high as
-// possible until the provided root directory is reached.
+// similar to both [kcl-lang.io/kcl-go/pkg/utils.FindPkgRoot] and
+// [kcl-lang.io/kcl-go/pkg/tools/list.FindPkgInfo], but adds additional guards
+// to prevent resolving outside the provided root. It also searches downward
+// from the provided root and returns the first match, rather than searching
+// upward from the provided path until the filesystem root.
 func FindTopPkgRoot(root, path string) (string, error) {
-	return findTopFile(root, path, "kcl.mod", false)
+	target := "kcl.mod"
+	f, err := findTopFile(root, path, func(s string) (bool, error) {
+		checkPath := filepath.Join(s, "kcl.mod")
+		fi, err := os.Lstat(checkPath)
+		if err != nil {
+			return false, fmt.Errorf("%s: %w", checkPath, err)
+		}
+		if fi.IsDir() {
+			return false, nil
+		}
+		return true, nil
+	})
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", target, err)
+	}
+	return f, nil
 }
 
 // FindRepoRoot returns topmost (i.e. passing submodules) git repository for the
 // provided path. If no git repository is found, it will return an error.
 func FindRepoRoot(path string) (string, error) {
-	return findTopFile("/", path, ".git", true)
+	// Look for a `.git` directory containing a `HEAD` file.
+	// Ideally this would be `git rev-parse --show-toplevel` but I didn't want to
+	// add another package just for this. To see what is normally looked up:
+	// sudo ktrace trace -S -f C3 -c git rev-parse --show-toplevel | grep .git/
+	target1 := ".git"
+	target2 := "HEAD"
+	f, err := findTopFile("/", path, func(s string) (bool, error) {
+		checkPath1 := filepath.Join(s, target1)
+		fi1, err := os.Lstat(checkPath1)
+		if err != nil {
+			return false, fmt.Errorf("%s: %w", checkPath1, err)
+		}
+		if !fi1.IsDir() {
+			return false, nil
+		}
+		checkPath2 := filepath.Join(s, target1, target2)
+		fi2, err := os.Lstat(checkPath2)
+		if err != nil {
+			return false, fmt.Errorf("%s: %w", checkPath2, err)
+		}
+		if fi2.IsDir() {
+			return false, nil
+		}
+		return true, nil
+	})
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", filepath.Join(target1, target2), err)
+	}
+	return f, nil
 }
 
-func findTopFile(root, path, target string, isDir bool) (string, error) {
+func findTopFile(root, path string, test func(string) (bool, error)) (string, error) {
 	rootAbs, err := filepath.Abs(root)
 	if err != nil {
 		return "", fmt.Errorf("failed to get absolute path: %w", err)
@@ -46,11 +92,10 @@ func findTopFile(root, path, target string, isDir bool) (string, error) {
 	currentDir := rootAbs
 	for _, part := range strings.Split(pathRel, "/") {
 		currentDir = filepath.Join(currentDir, part)
-		checkPath := filepath.Join(currentDir, target)
-		if fi, err := os.Lstat(checkPath); err == nil && isDir == fi.IsDir() {
+		if match, err := test(currentDir); err == nil && match {
 			return currentDir, nil
 		}
 	}
 
-	return "", fmt.Errorf("%s: %w", target, ErrFileNotFound)
+	return "", ErrFileNotFound
 }
