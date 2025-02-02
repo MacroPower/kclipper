ad7f9b3537eae12f5f53a65c204bb9f971cd9fa6
Jacob Colvin <jacobcolvin1@gmail.com>
2025-02-01 22:55:16 -0500

    Fix devbox permissions

--- .github/renovate-entrypoint.sh
+++ .github/renovate-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
+chmod +x ~/.nix-profile/etc/profile.d/nix.sh
 bash ~/.nix-profile/etc/profile.d/nix.sh
 
 export PATH="/home/${DEVBOX_USER}/.nix-profile/bin:$PATH"
@@ -8,7 +9,8 @@ export PATH="/home/${DEVBOX_USER}/.nix-profile/bin:$PATH"
 # Step 3: Installing devbox
 export DEVBOX_USE_VERSION="0.13.7"
 curl -L https://get.jetify.com/devbox | bash -s -- -f
-chown -R "${DEVBOX_USER}:${DEVBOX_USER}" /usr/local/bin/devbox
+chown -R ubuntu:ubuntu /usr/local/bin/devbox
+chmod +x /usr/local/bin/devbox
 
 KCLIPPER_URL=$(curl -s "https://api.github.com/repos/MacroPower/kclipper/releases/latest" | \
   jq -r ".assets[] | select(.name | test(\"kclipper_$(uname)_$(arch).tar.gz\")) | .browser_download_url")
