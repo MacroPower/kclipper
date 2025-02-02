c8e2e1dad0c22789deab4de6fa72df871fdf9491
Jacob Colvin <jacobcolvin1@gmail.com>
2025-02-01 22:36:13 -0500

    Set up devbox for renovate

--- .github/renovate-entrypoint.sh
+++ .github/renovate-entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
+bash ~/.nix-profile/etc/profile.d/nix.sh
+
+export PATH="/home/${DEVBOX_USER}/.nix-profile/bin:$PATH"
+
+# Step 3: Installing devbox
+export DEVBOX_USE_VERSION="0.13.7"
+curl -L https://get.jetify.com/devbox | bash -s -- -f
+chown -R "${DEVBOX_USER}:${DEVBOX_USER}" /usr/local/bin/devbox
+
 KCLIPPER_URL=$(curl -s "https://api.github.com/repos/MacroPower/kclipper/releases/latest" | \
   jq -r ".assets[] | select(.name | test(\"kclipper_$(uname)_$(arch).tar.gz\")) | .browser_download_url")
 
