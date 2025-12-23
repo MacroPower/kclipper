1f1197d300e8dd6cd0a2e80dd734f9654999db57
Jacob Colvin <jacobcolvin1@gmail.com>
2025-12-23 14:52:39 -0500

    fix(deps): update go to 1.25

--- hack/zig-gold-wrapper.sh
+++ hack/zig-gold-wrapper.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 # Wrapper script to make zig appear as gold linker to Go
 
-# If being asked for version info, pretend to be gold
-if [[ "$*" == *"--version"* ]] || [[ "$*" == *"-v"* ]]; then
-    echo "GNU gold (GNU Binutils 2.40) 1.16"
-    exit 0
-fi
+args=()
+for arg in "$@"; do
+    case "$arg" in
+        --version|-v|-Wl,--version)
+            # If being asked for version info, pretend to be gold
+            echo "GNU gold (GNU Binutils 2.40) 1.16"
+            exit 0
+            ;;
+        -fuse-ld=gold)
+            # Go explicitly requests gold; skip this flag as Zig uses LLD
+            ;;
+        -plugin=*|-plugin|-plugin-opt=*|-plugin-opt|-Wl,-plugin*|-Wl,--plugin*)
+            # Skip gold linker plugin flags
+            ;;
+        *)
+            args+=("$arg")
+            ;;
+    esac
+done
 
 # Use zig cc as the linker
-exec zig cc "$@"
+exec zig cc "${args[@]}"
